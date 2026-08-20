3ffa27ce705b16adfd28d0fc3ce99b0968758374
増田悠汰 <yuutantan.ma@gmail.com>
2026-08-20 06:48:45 +0000

    ジャンプとダッシュを追加した

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -7,6 +7,15 @@ export class PlayScene extends Phaser.Scene {
 
   private player!: Phaser.GameObjects.Rectangle;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private jumpKey!: Phaser.Input.Keyboard.Key;
+  private dashKey!: Phaser.Input.Keyboard.Key;
+  private playerVelocityY = 0;
+  private isGrounded = false;
+  private groundY = 0;
+  private jumpHoldTimer = 0;
+  private dashTimer = 0;
+  private dashDirection = 0;
+  private facingDirection = 1;
 
   create(): void {
     const tileSize = 64;
@@ -16,6 +25,7 @@ export class PlayScene extends Phaser.Scene {
     const terrainHeight = rows * tileSize;
     const startX = this.scale.width / 2 - terrainWidth / 2;
     const startY = this.scale.height - terrainHeight;
+    this.groundY = startY;
 
     for (let row = 0; row < rows; row++) {
       for (let col = 0; col < cols; col++) {
@@ -39,12 +49,16 @@ export class PlayScene extends Phaser.Scene {
       1,
     ).setStrokeStyle(3, 0x000000);
 
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.input.keyboard?.on('keydown-ESC', () => {
-      this.scene.pause();
-      this.scene.launch('PauseScene');
-    });
+    const keyboard = this.input.keyboard;
+    if (keyboard) {
+      this.cursors = keyboard.createCursorKeys();
+      this.jumpKey = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+      this.dashKey = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
+      keyboard.on('keydown-ESC', () => {
+        this.scene.pause();
+        this.scene.launch('PauseScene');
+      });
+    }
   }
 
   update(): void {
@@ -54,14 +68,70 @@ export class PlayScene extends Phaser.Scene {
     const startX = this.scale.width / 2 - terrainWidth / 2;
     const minX = startX + tileSize / 2;
     const maxX = startX + terrainWidth - tileSize / 2;
-    const moveSpeed = 280;
+    const moveSpeed = tileSize * 6;
+    const dashDistance = tileSize * 6;
+    const dashDuration = 0.1;
+    const dashSpeed = dashDistance / dashDuration;
+    const gravity = 3200;
+    const jumpCells = 4;
+    const jumpHeight = tileSize * jumpCells;
+    const jumpVelocity = (tileSize * jumpCells) / 0.05;
+    const deltaSeconds = this.game.loop.delta / 1000;
+    const groundTop = this.groundY - this.player.height / 2;
 
     if (this.cursors.left?.isDown) {
-      this.player.x = Phaser.Math.Clamp(this.player.x - moveSpeed * this.game.loop.delta / 1000, minX, maxX);
+      this.facingDirection = -1;
     }
 
     if (this.cursors.right?.isDown) {
-      this.player.x = Phaser.Math.Clamp(this.player.x + moveSpeed * this.game.loop.delta / 1000, minX, maxX);
+      this.facingDirection = 1;
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(this.dashKey) && this.dashTimer <= 0) {
+      this.dashDirection = this.cursors.left?.isDown ? -1 : this.cursors.right?.isDown ? 1 : this.facingDirection;
+      this.dashTimer = dashDuration;
+    }
+
+    if (this.dashTimer > 0) {
+      this.player.x += this.dashDirection * dashSpeed * deltaSeconds;
+      this.player.x = Phaser.Math.Clamp(this.player.x, minX, maxX);
+      this.dashTimer = Math.max(0, this.dashTimer - deltaSeconds);
+    } else {
+      if (this.cursors.left?.isDown) {
+        this.player.x = Phaser.Math.Clamp(this.player.x - moveSpeed * deltaSeconds, minX, maxX);
+      }
+
+      if (this.cursors.right?.isDown) {
+        this.player.x = Phaser.Math.Clamp(this.player.x + moveSpeed * deltaSeconds, minX, maxX);
+      }
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(this.jumpKey) && this.isGrounded) {
+      this.playerVelocityY = -jumpVelocity;
+      this.jumpHoldTimer = 0.1;
+      this.isGrounded = false;
+    }
+
+    if (this.player.y <= groundTop - jumpHeight) {
+      if (this.jumpHoldTimer > 0) {
+        this.jumpHoldTimer -= deltaSeconds;
+        this.playerVelocityY = 0;
+      } else {
+        this.playerVelocityY += gravity * deltaSeconds;
+      }
+    } else {
+      this.playerVelocityY += gravity * deltaSeconds;
+    }
+
+    this.player.y += this.playerVelocityY * deltaSeconds;
+
+    if (this.player.y >= groundTop) {
+      this.player.y = groundTop;
+      this.playerVelocityY = 0;
+      this.isGrounded = true;
+      this.jumpHoldTimer = 0;
+    } else {
+      this.isGrounded = false;
     }
   }
 }
