3c2421eed8794332b8d00651bda650ecb68168cd
増田悠汰 <yuutantan.ma@gmail.com>
2026-08-20 06:23:16 +0000

    左右に動けるようにした

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -5,6 +5,9 @@ export class PlayScene extends Phaser.Scene {
     super('PlayScene');
   }
 
+  private player!: Phaser.GameObjects.Rectangle;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+
   create(): void {
     const tileSize = 64;
     const cols = 10;
@@ -27,9 +30,38 @@ export class PlayScene extends Phaser.Scene {
       }
     }
 
+    this.player = this.add.rectangle(
+      startX + tileSize / 2,
+      startY - tileSize / 2,
+      tileSize,
+      tileSize,
+      0xff0000,
+      1,
+    ).setStrokeStyle(3, 0x000000);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
     this.input.keyboard?.on('keydown-ESC', () => {
       this.scene.pause();
       this.scene.launch('PauseScene');
     });
+  }
+
+  update(): void {
+    const tileSize = 64;
+    const cols = 10;
+    const terrainWidth = cols * tileSize;
+    const startX = this.scale.width / 2 - terrainWidth / 2;
+    const minX = startX + tileSize / 2;
+    const maxX = startX + terrainWidth - tileSize / 2;
+    const moveSpeed = 280;
+
+    if (this.cursors.left?.isDown) {
+      this.player.x = Phaser.Math.Clamp(this.player.x - moveSpeed * this.game.loop.delta / 1000, minX, maxX);
+    }
+
+    if (this.cursors.right?.isDown) {
+      this.player.x = Phaser.Math.Clamp(this.player.x + moveSpeed * this.game.loop.delta / 1000, minX, maxX);
+    }
   }
 }
